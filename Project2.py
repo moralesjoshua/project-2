a0980269337aa8933ce026264fd6569b3ad0ca39
moralesjoshua <151475871+moralesjoshua@users.noreply.github.com>
2023-12-08 21:34:55 -0600

    Beginning

--- Project2.py
+++ Project2.py
@@ -4,7 +4,7 @@ from guiProject2 import *
 def main():
     window = Tk()
     window.title('Calculator')
-    window.geometry('500x400')
+    window.geometry('300x420')
     window.resizable(False, False)
     Calculator(window)
     window.mainloop()
